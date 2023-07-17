ef791d473e0fe0cb89ed319b6d553612530cc6fa
NikolaKasabov <n_kasabov@mail.bg>
2023-07-17 12:34:03 +0300

    add state and event handlers

--- project-2/src/App.js
+++ project-2/src/App.js
@@ -1,13 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
+
 import Card from './components/Card';
 import classes from './App.module.css';
 import AddUser from './components/AddUser';
 
 function App() {
+  const [formData, setFormData] = useState({
+    name: '',
+    age: '',
+  });
+
+  function handleChange(ev) {
+    const { name, value } = ev.target;
+    setFormData(old => ({
+      ...old,
+      [name]: value,
+    }));
+  }
+
+  function handleSubmit(ev) {
+    ev.preventDefault();
+  }
+
   return (
     <div>
       <Card className={classes.formCard}>
-        <AddUser />
+        <AddUser
+          values={formData}
+          onChange={handleChange}
+          onSubmit={handleSubmit}
+        />
       </Card>
     </div>
   );
