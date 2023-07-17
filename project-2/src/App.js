34991d5cfcb1b900197e650f2510460b4e3cee08
NikolaKasabov <n_kasabov@mail.bg>
2023-07-17 09:56:23 +0300

    add Card component

--- project-2/src/App.js
+++ project-2/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
-
+import Card from './components/Card';
+import classes from './App.module.css';
 
 function App() {
   return (
     <div>
-
+      
     </div>
   );
 }
---
+++ project-2/src/components/Card.js
@@ -0,0 +1,9 @@
+import classes from './Card.module.css';
+
+function Card({ className, children }) {
+  return (
+    <div className={`${className} ${classes.card}`}>{children}</div>
+  );
+}
+
+export default Card;
